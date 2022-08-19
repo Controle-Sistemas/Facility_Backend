34d168da8237b9e43c3389a7d6ee9dc35fabd52d
Controle-Sistemas <controlesistemasgithub@gmail.com>
2022-08-19 10:14:36 -0300

    testando

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,10 +1,10 @@
 import mysql from 'mysql2'
-import dotenv from 'dotenv'
-dotenv.config()
+// import dotenv from 'dotenv'
+// dotenv.config()
 let connection: mysql.Connection;
 
-if(process.env.ENVIRONMENT === 'PROD') {
-    console.log('PROD');
+// if(process.env.ENVIRONMENT === 'PROD') {
+//     console.log('PROD');
     connection = mysql.createConnection({
         host: "us-cdbr-east-06.cleardb.net",
         user: "b7d2057b60237e",
@@ -12,15 +12,15 @@ if(process.env.ENVIRONMENT === 'PROD') {
         database: "heroku_b720c4485f58516",
         connectTimeout: 60 * 60 * 1000,
     })
-} else {
-    connection = mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        password: 'controle*302109',
-        database: 'projeto-portal',
-        port: 3306
-    })
-}
+// } else {
+//     connection = mysql.createConnection({
+//         host: 'localhost',
+//         user: 'root',
+//         password: 'controle*302109',
+//         database: 'projeto-portal',
+//         port: 3306
+//     })
+// }
 
 
 
