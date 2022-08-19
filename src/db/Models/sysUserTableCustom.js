743d729afdc9fa50c11efa1f837b6788a5d3f1d7
Controle-Sistemas <controlesistemasgithub@gmail.com>
2022-08-19 15:00:48 -0300

    verificando erros

--- src/db/Models/sysUserTableCustom.js
+++ src/db/Models/sysUserTableCustom.js
@@ -6,54 +6,99 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const __1 = __importDefault(require(".."));
 class UserTableCustomModel {
     editTable(tableData, idUser, res) {
-        __1.default.query(`UPDATE sysselecttablecolumn SET ? WHERE  idUser = ${idUser} and idTable = ${tableData.idTable} AND id = ${tableData.id}`, [tableData]);
+        try {
+            __1.default.query(`UPDATE sysselecttablecolumn SET ? WHERE  idUser = ${idUser} and idTable = ${tableData.idTable} AND id = ${tableData.id}`, [tableData]);
+        }
+        catch (error) {
+            res.status(500).send(error);
+            console.log(error);
+        }
+        finally {
+            __1.default.end();
+        }
     }
     getTable(idUser, idTable, res) {
-        __1.default.query(`SELECT * FROM sysselecttablecolumn WHERE idUser = ${idUser} AND idTable = ${idTable}`, (err, results) => {
-            if (err) {
-                res.status(400).send({
-                    message: err
-                });
-            }
-            else {
-                console.log(typeof results);
-                if (results.toString().length > 0) {
-                    res.status(200).send({
-                        message: 'Tabela listada com sucesso',
-                        data: results
+        try {
+            __1.default.query(`SELECT * FROM sysselecttablecolumn WHERE idUser = ${idUser} AND idTable = ${idTable}`, (err, results) => {
+                if (err) {
+                    res.status(400).send({
+                        message: err
                     });
                 }
                 else {
-                    res.status(404).send({
-                        message: 'Tabela não encontrada'
-                    });
+                    console.log(typeof results);
+                    if (results.toString().length > 0) {
+                        res.status(200).send({
+                            message: 'Tabela listada com sucesso',
+                            data: results
+                        });
+                    }
+                    else {
+                        res.status(404).send({
+                            message: 'Tabela não encontrada'
+                        });
+                    }
                 }
-            }
-        });
+            });
+        }
+        catch (error) {
+            res.status(500).send(error);
+            console.log(error);
+        }
+        finally {
+            __1.default.end();
+        }
     }
     createTable(tableData, res) {
-        __1.default.query(`INSERT INTO sysselecttablecolumn SET ?`, [tableData], (err) => {
-            if (err) {
-                console.log(err);
-            }
-        });
+        try {
+            __1.default.query(`INSERT INTO sysselecttablecolumn SET ?`, [tableData], (err) => {
+                if (err) {
+                    console.log(err);
+                }
+            });
+        }
+        catch (error) {
+            res.status(500).send(error);
+            console.log(error);
+        }
+        finally {
+            __1.default.end();
+        }
     }
     deleteTable(idTable, res) {
-        __1.default.query(`DELETE FROM sysselecttablecolumn WHERE idUser = ${idTable}`, (err) => {
-            if (err) {
-                res.status(400).send({
-                    message: err
-                });
-            }
-            else {
-                res.status(200).send({
-                    message: 'Tabela deletada com sucesso'
-                });
-            }
-        });
+        try {
+            __1.default.query(`DELETE FROM sysselecttablecolumn WHERE idUser = ${idTable}`, (err) => {
+                if (err) {
+                    res.status(400).send({
+                        message: err
+                    });
+                }
+                else {
+                    res.status(200).send({
+                        message: 'Tabela deletada com sucesso'
+                    });
+                }
+            });
+        }
+        catch (error) {
+            res.status(500).send(error);
+            console.log(error);
+        }
+        finally {
+            __1.default.end();
+        }
     }
     deleteTableByUser(idUser, res) {
-        __1.default.query(`DELETE FROM sysselecttablecolumn WHERE  idUser = ${idUser}`);
+        try {
+            __1.default.query(`DELETE FROM sysselecttablecolumn WHERE  idUser = ${idUser}`);
+        }
+        catch (error) {
+            res.status(500).send(error);
+            console.log(error);
+        }
+        finally {
+            __1.default.end();
+        }
     }
 }
 exports.default = new UserTableCustomModel();
--- src/db/Models/sysUserTableCustom.ts
+++ src/db/Models/sysUserTableCustom.ts
@@ -1,77 +1,99 @@
-import conn from '..'
-import {  Response } from 'express';
+import conn from '..';
+import { Response } from 'express';
 
 class UserTableCustomModel {
+	editTable(tableData: any, idUser: number, res: Response) {
+		try {
+			conn.query(
+				`UPDATE sysselecttablecolumn SET ? WHERE  idUser = ${idUser} and idTable = ${tableData.idTable} AND id = ${tableData.id}`,
+				[ tableData ]
+			);
+		} catch (error) {
+			res.status(500).send(error);
+			console.log(error);
+		} finally {
+			conn.end();
+		}
+	}
 
-    editTable(tableData: any,idUser:number,res:Response) {
-        
-        conn.query(
-            `UPDATE sysselecttablecolumn SET ? WHERE  idUser = ${idUser} and idTable = ${tableData.idTable} AND id = ${tableData.id}`,[tableData],
-        )
-    }
+	getTable(idUser: number, idTable: number, res: Response) {
+		try {
+			conn.query(
+				`SELECT * FROM sysselecttablecolumn WHERE idUser = ${idUser} AND idTable = ${idTable}`,
+				(err, results) => {
+					if (err) {
+						res.status(400).send({
+							message: err
+						});
+					} else {
+						console.log(typeof results);
+						if (results.toString().length > 0) {
+							res.status(200).send({
+								message: 'Tabela listada com sucesso',
+								data: results
+							});
+						} else {
+							res.status(404).send({
+								message: 'Tabela não encontrada'
+							});
+						}
+					}
+				}
+			);
+		} catch (error) {
+			res.status(500).send(error);
+			console.log(error);
+		} finally {
+			conn.end();
+		}
+	}
 
-    getTable(idUser:number,idTable:number,res:Response) {
-        conn.query(
-            `SELECT * FROM sysselecttablecolumn WHERE idUser = ${idUser} AND idTable = ${idTable}`,
-            (err,results) => {
-                if (err) {
-                    res.status(400).send({
-                        message: err
+	createTable(tableData: any, res: Response) {
+		try {
+			conn.query(`INSERT INTO sysselecttablecolumn SET ?`, [ tableData ], (err) => {
+				if (err) {
+					console.log(err);
+				}
+			});
+		} catch (error) {
+			res.status(500).send(error);
+			console.log(error);
+		} finally {
+			conn.end();
+		}
+	}
 
-                    })
-                } else {
-                    console.log(typeof results)
-                    if(results.toString().length > 0){
-                        res.status(200).send({
-                            message: 'Tabela listada com sucesso',
-                            data: results
-                        })
-                    } else {
-                        res.status(404).send({
-                            message: 'Tabela não encontrada'
-                        })
-                    }
-                }
-            }	
-        );
-    }
+	deleteTable(idTable: number, res: Response) {
+		try {
+			conn.query(`DELETE FROM sysselecttablecolumn WHERE idUser = ${idTable}`, (err) => {
+				if (err) {
+					res.status(400).send({
+						message: err
+					});
+				} else {
+					res.status(200).send({
+						message: 'Tabela deletada com sucesso'
+					});
+				}
+			});
+		} catch (error) {
+			res.status(500).send(error);
+			console.log(error);
+		} finally {
+			conn.end();
+		}
+	}
 
-    createTable(tableData: any,res:Response) {
-        conn.query(
-            `INSERT INTO sysselecttablecolumn SET ?`,[tableData],
-            (err) => {
-                if (err) {
-                    console.log(err)
-                }
-            }	
-        );
-    }
-
-    deleteTable(idTable:number,res:Response) {
-        conn.query(
-            `DELETE FROM sysselecttablecolumn WHERE idUser = ${idTable}`,
-            (err) => {
-                if (err) {
-                    res.status(400).send({
-                        message: err
-
-                    })
-                } else {
-                    res.status(200).send({
-                        message: 'Tabela deletada com sucesso'
-                    })
-                }
-            }	
-        );
-    }
-
-    deleteTableByUser(idUser:number,res:Response) {
-        conn.query(
-            `DELETE FROM sysselecttablecolumn WHERE  idUser = ${idUser}`);
-    }
-
-   
-
+	deleteTableByUser(idUser: number, res: Response) {
+		try {
+			conn.query(`DELETE FROM sysselecttablecolumn WHERE  idUser = ${idUser}`);
+		} catch (error) {
+			res.status(500).send(error);
+			console.log(error);
+		} finally {
+			conn.end();
+		}
+	}
 }
 
 export default new UserTableCustomModel();
