0668fed6c48961620d5eab2678b2d24520687f34
landwixxx <landwixxx@gmail.com>
2022-10-08 13:14:45 -0300

    Antes build

--- src/routes/Controllers/rotasRequisicaoDashboard.ts
+++ src/routes/Controllers/rotasRequisicaoDashboard.ts
@@ -5,7 +5,7 @@ const routes = express.Router()
 routes.get('/', (req: Request, res: Response) => {
 
     const dashboardRequest = new DashboardRequest('@20033038')
-    const dataPromise = dashboardRequest.getData()
+    const dataPromise = dashboardRequest.getRealTime()
     console.log('Entrou na Promisse')
     Promise.resolve(dataPromise).then(response => {
         if(response.error){
@@ -24,5 +24,27 @@ routes.get('/', (req: Request, res: Response) => {
     })
 
 })
+
+routes.get('/real-time', (req: Request, res: Response) => {
+    
+    const dashboardRequest = new DashboardRequest(req.params.clientId)
+    const dataPromise = dashboardRequest.getRealTime()
+    console.log('Entrou na Promisse')
+    Promise.resolve(dataPromise).then(response => {
+        if(response.error){
+            res.status(400).json({
+                message:"Erro!",
+                data:response.data,
+            })
+        } else {
+            console.log(response)
+            return res.status(200).json({ message: `Dados em tempo real recuperados com sucesso`, data:response})
+
+        }
+
+    })
+
+})
+
 
 export default routes;
--- src/routes/requests/dashboard.ts
+++ src/routes/requests/dashboard.ts
@@ -6,25 +6,24 @@ class DashboardRequest{
     constructor(clientId:string){
         this.clientId = clientId
     }
-    getActualMonthData(id:number){
-        return axios.get(`${API_PRODUCT_URL}/VWSALESPERMONTH`,{
+    async getSalesPerMonth () {
+        const response = await axios.get(`${API_PRODUCT_URL}/VWSALESPERMONTH`,{
             headers:{
                 "socket_client":this.clientId
-            },
-            data:{
-                "ClientId":this.clientId,"id":id
-            }
-        }).then(response => {
-            return {res:response, error: false}
-        }).catch(err => {
-            return {res:err, error: true}
+            }}).then(response => {
+            console.log(response)
+            return response.data
         })
-    }
+        .catch(error => {
+            console.log(error)
+        })
+        return response
+}
        
-    async getData () {
-            const response = await axios.get(`${API_PRODUCT_URL}/VWSALESPERMONTH`,{
+    async getRealTime () {
+            const response = await axios.get(`${API_PRODUCT_URL}/RealTime`,{
                 headers:{
-                    "socket_client":this.clientId
+                    "socket_client":'@20033038' // '@' + this.clientId
                 }}).then(response => {
                 console.log(response)
                 return response.data
@@ -33,9 +32,7 @@ class DashboardRequest{
                 console.log(error)
             })
             return response
-        
-            
-          }
+    }
 }
 
 
