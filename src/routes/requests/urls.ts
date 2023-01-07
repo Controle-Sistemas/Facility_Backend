e3717f3326cf215be0b63e71a436598558283b46
Landwixxx <landwixxx@gmail.com>
2023-01-07 13:38:15 -0300

    Novo IP servidor

--- src/routes/requests/urls.ts
+++ src/routes/requests/urls.ts
@@ -1,3 +1,3 @@
 export const EXTERNAL_API_URL = `https://apilicenca.controleautomacao.com.br/api/`;
 
-export const API_PRODUCT_URL = 'http://192.95.42.179:9000/socket'
+export const API_PRODUCT_URL = 'http://167.114.135.197:9000/socket'
